6df731669049c3ac61cd28569c13ddd91eb4da9d
Mateus Junio <mateus-gzn@hotmail.com>
2022-08-26 03:58:58 -0300

    Curso Java Poo modificadores de acesso.

--- src/aula03/Caneta.java
+++ src/aula03/Caneta.java
@@ -1,34 +1,30 @@
-package aula02;
+package aula03;
 
 public class Caneta {
-    public String modelo;
-    public String cor;
-    private float ponta;
-    protected int carga;
-    private boolean tampada;
-
-    public void status() {
+   public String modelo;
+   public String cor;
+   private float ponta;
+   protected int carga;
+   protected boolean tampada;
+   public void status(){
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Uma caneta " + this.cor);
         System.out.println("Ponta " + this.ponta);
         System.out.println("Carga " + this.carga);
         System.out.println("Está tampada " + this.tampada);
     }
-
-    protected void rabiscar() {
+    public void rabiscar(){
         if (this.tampada == true) {
             System.out.println("Erro não posso rabiscar.");
-        } else {
+        } else{
             System.out.println("Estou rabiscando.");
         }
     }
-
-    public void tampar() {
+   protected void tampar(){
         this.tampada = true;
     }
-
-    public void destampar() {
+   protected void destampar(){
         this.tampada = false;
     }
-
 }
+
